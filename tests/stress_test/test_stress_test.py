46fa81f9d78039220aba99ddfd2869c737ce3976
Phavarisa Limchitti <aom.limchitti@gmail.com>
2025-08-14 10:00:38 +0700

    chore: add docstring to schema module

--- tests/stress_test/test_stress_test.py
+++ tests/stress_test/test_stress_test.py
@@ -1,27 +1,32 @@
 import logging
-import random
-import pytest
 import time
+
 import numpy as np
-from comma_fixer.schema import Schema
+import pytest
+
 from comma_fixer.fixer import Fixer
+from comma_fixer.schema import Schema
 
 logger = logging.getLogger("Stress Test Logs")
 
-def run_shortest_path_on_n_tokens_by_n_columns(fixer: Fixer, n_token: int, n_col: int, seed: int):
+
+def run_shortest_path_on_n_tokens_by_n_columns(
+    fixer: Fixer, n_token: int, n_col: int, seed: int
+):
     if n_col > n_token:
         print("Invalid n_token x n_col size!")
         return
-    
+
     np.random.seed(seed)
-    
+
     v_tc = np.random.randint(2, size=(n_token, n_col))
     np.fill_diagonal(v_tc, 0)
     start = time.time()
-    res = fixer._Fixer__find_shortest_paths(v_tc)
+    _ = fixer._Fixer__find_shortest_paths(v_tc)
     end = time.time()
     logger.info(f"{end-start}")
     return end - start
+
 
 @pytest.mark.stress_test
 class TestShortestPath:
@@ -29,20 +34,28 @@ class TestShortestPath:
 
     def test_50_tokens_by_40_columns(self):
         fixer = Fixer.new(Schema.new_schema())
-        execution_time = run_shortest_path_on_n_tokens_by_n_columns(fixer, 50, 40, self.seed)
+        execution_time = run_shortest_path_on_n_tokens_by_n_columns(
+            fixer, 50, 40, self.seed
+        )
         assert execution_time < 1
 
     def test_100_tokens_by_100_columns(self):
         fixer = Fixer.new(Schema.new_schema())
-        execution_time = run_shortest_path_on_n_tokens_by_n_columns(fixer, 100, 100, self.seed)
+        execution_time = run_shortest_path_on_n_tokens_by_n_columns(
+            fixer, 100, 100, self.seed
+        )
         assert execution_time < 1
 
     def test_60_tokens_by_30_columns(self):
         fixer = Fixer.new(Schema.new_schema())
-        execution_time = run_shortest_path_on_n_tokens_by_n_columns(fixer, 60, 30, self.seed)
+        execution_time = run_shortest_path_on_n_tokens_by_n_columns(
+            fixer, 60, 30, self.seed
+        )
         assert execution_time < 1
 
     def test_1000_tokens_by_1000_columns(self):
         fixer = Fixer.new(Schema.new_schema())
-        execution_time = run_shortest_path_on_n_tokens_by_n_columns(fixer, 1000, 1000, self.seed)
+        execution_time = run_shortest_path_on_n_tokens_by_n_columns(
+            fixer, 1000, 1000, self.seed
+        )
         assert execution_time < 10
