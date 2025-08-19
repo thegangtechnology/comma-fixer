c2a83966a98a429a6eaf9052f530690a4143e445
Phavarisa Limchitti <aom.limchitti@gmail.com>
2025-08-19 15:07:31 +0700

    fix: account for whether first line was skipped in input file which influenced line indexing when exporting to csv

--- src/comma_fixer/parsed.py
+++ src/comma_fixer/parsed.py
@@ -1,4 +1,5 @@
 import csv
+import logging
 from dataclasses import dataclass
 from typing import TypeAlias
 
@@ -8,6 +9,7 @@ from comma_fixer.schema import Schema
 
 InvalidEntry: TypeAlias = tuple[int, str]
 ParsedEntry: TypeAlias = list[str]
+logger = logging.getLogger("Parsed Logs")
 
 
 @dataclass
@@ -16,30 +18,42 @@ class Parsed:
     Parsed class produced from calling `fix_file` in Fixer.
 
     Attributes:
-        schema (Schema): Schema of dataset.
-        processed (str): CSV of dataset stored as a string.
-        invalid_line_numbers (list[InvalidEntry]): List of invalid entries containing line number and entry.
+        _schema (Schema): Schema of dataset.
+        _processed (str): CSV of dataset stored as a string.
+        _invalid_line_numbers (list[InvalidEntry]): List of invalid entries containing line number and entry.
+        _skip_first_line (bool): Whether the original CSV file had headers as the first row.
     """
 
     _schema: Schema
     _processed: str
     _invalid_entries: list[InvalidEntry]
+    _skip_first_line: bool
 
     @classmethod
     def new(
-        cls, schema: Schema, processed_csv: str, invalid_entries: list[InvalidEntry]
+        cls,
+        schema: Schema,
+        processed_csv: str,
+        invalid_entries: list[InvalidEntry],
+        skip_first_line: bool,
     ) -> "Parsed":
         """
         Creates an empty Parsed object with specified Schema.
 
         Args:
             schema (Schema): Schema of dataset.
+            processed_csv (str): Processed CSV as a string.
+            invalid_entries (list[InvalidEntry]): List of invalid entries with line index.
+            skip_first_line (bool): Whether the first line was skipped in original input file.
 
         Returns:
             Parsed. Parsed object with specified schema.
         """
         return Parsed(
-            _schema=schema, _processed=processed_csv, _invalid_entries=invalid_entries
+            _schema=schema,
+            _processed=processed_csv,
+            _invalid_entries=invalid_entries,
+            _skip_first_line=skip_first_line,
         )
 
     def export_to_csv_best_effort(self, filepath: str):
@@ -50,9 +64,12 @@ class Parsed:
         """
         processed_to_csv = pd.DataFrame(self._schema.get_series_dict())
         for line_number, line in enumerate(self._processed.split("\n")):
+            if self._skip_first_line:
+                line_number += 1
             if (line_number, line) not in self._invalid_entries:
                 parsed_csv = list(csv.reader([line]))[0]
                 processed_to_csv.loc[len(processed_to_csv)] = parsed_csv
+        logger.info(processed_to_csv.info())
         return processed_to_csv.to_csv(filepath, index=False)
 
     def print_all_invalid_entries(self):
